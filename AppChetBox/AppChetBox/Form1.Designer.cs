c8897fb14f61f5d6c8f3f7f8a073985928417f3a
hebertmedelo <h.hantonio@hotmail.com>
2015-04-15 21:42:20 -0500

    entrga4
    entraga 4 visual.net

---
+++ AppChetBox/AppChetBox/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AppChetBox
+{
+    partial class btnselecionar
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkopcion1 = new System.Windows.Forms.CheckBox();
+            this.chkopcion2 = new System.Windows.Forms.CheckBox();
+            this.chkopcion3 = new System.Windows.Forms.CheckBox();
+            this.btnseleccionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkopcion1
+            // 
+            this.chkopcion1.AutoSize = true;
+            this.chkopcion1.Location = new System.Drawing.Point(36, 25);
+            this.chkopcion1.Name = "chkopcion1";
+            this.chkopcion1.Size = new System.Drawing.Size(61, 17);
+            this.chkopcion1.TabIndex = 0;
+            this.chkopcion1.Text = "amarillo";
+            this.chkopcion1.UseVisualStyleBackColor = true;
+            this.chkopcion1.CheckedChanged += new System.EventHandler(this.chkopcion1_CheckedChanged);
+            // 
+            // chkopcion2
+            // 
+            this.chkopcion2.AutoSize = true;
+            this.chkopcion2.Location = new System.Drawing.Point(36, 59);
+            this.chkopcion2.Name = "chkopcion2";
+            this.chkopcion2.Size = new System.Drawing.Size(45, 17);
+            this.chkopcion2.TabIndex = 1;
+            this.chkopcion2.Text = "azul";
+            this.chkopcion2.UseVisualStyleBackColor = true;
+            // 
+            // chkopcion3
+            // 
+            this.chkopcion3.AutoSize = true;
+            this.chkopcion3.Location = new System.Drawing.Point(36, 92);
+            this.chkopcion3.Name = "chkopcion3";
+            this.chkopcion3.Size = new System.Drawing.Size(43, 17);
+            this.chkopcion3.TabIndex = 2;
+            this.chkopcion3.Text = "rojo";
+            this.chkopcion3.UseVisualStyleBackColor = true;
+            this.chkopcion3.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnseleccionar
+            // 
+            this.btnseleccionar.Location = new System.Drawing.Point(36, 144);
+            this.btnseleccionar.Name = "btnseleccionar";
+            this.btnseleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnseleccionar.TabIndex = 3;
+            this.btnseleccionar.Text = "seleccionar";
+            this.btnseleccionar.UseVisualStyleBackColor = true;
+            this.btnseleccionar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnselecionar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnseleccionar);
+            this.Controls.Add(this.chkopcion3);
+            this.Controls.Add(this.chkopcion2);
+            this.Controls.Add(this.chkopcion1);
+            this.Name = "btnselecionar";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.btnselecionar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkopcion1;
+        private System.Windows.Forms.CheckBox chkopcion2;
+        private System.Windows.Forms.CheckBox chkopcion3;
+        private System.Windows.Forms.Button btnseleccionar;
+    }
+}
+
---
+++ AppTexBox2/AppTexBox2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace AppTexBox2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblclave = new System.Windows.Forms.Label();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.btnverificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblclave
+            // 
+            this.lblclave.AutoSize = true;
+            this.lblclave.Location = new System.Drawing.Point(51, 41);
+            this.lblclave.Name = "lblclave";
+            this.lblclave.Size = new System.Drawing.Size(41, 13);
+            this.lblclave.TabIndex = 0;
+            this.lblclave.Text = "CLAVE";
+            // 
+            // txtclave
+            // 
+            this.txtclave.Location = new System.Drawing.Point(170, 41);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.Size = new System.Drawing.Size(100, 20);
+            this.txtclave.TabIndex = 1;
+            // 
+            // btnverificar
+            // 
+            this.btnverificar.Location = new System.Drawing.Point(103, 151);
+            this.btnverificar.Name = "btnverificar";
+            this.btnverificar.Size = new System.Drawing.Size(129, 23);
+            this.btnverificar.TabIndex = 2;
+            this.btnverificar.Text = "VERIFICAR CLAVE";
+            this.btnverificar.UseVisualStyleBackColor = true;
+            this.btnverificar.Click += new System.EventHandler(this.btnverificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 337);
+            this.Controls.Add(this.btnverificar);
+            this.Controls.Add(this.txtclave);
+            this.Controls.Add(this.lblclave);
+            this.Name = "Form1";
+            this.Text = "verificar clave";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblclave;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.Button btnverificar;
+    }
+}
+
